44a64e8cecbd80047236bca9a1435bb0527ff788
chaitanya <kolasanichaitanya@gmail.com>
2016-02-04 21:03:43 +0530

    Support to add update transactions to database

--- webtimenotify/updatescheduler/models.py
+++ webtimenotify/updatescheduler/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UpdateEvent(models.Model):
+	transaction_id = models.CharField(max_length=100)
+	no_of_hours = models.IntegerField(default=0)
+	job_number_choice = models.IntegerField(default=0)
--- webtimenotify/updatescheduler/views.py
+++ webtimenotify/updatescheduler/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from updatescheduler.models import UpdateEvent
 
 def index(request):
 	template = loader.get_template('entertask.html')
 	return HttpResponse(template.render(request))
 
-    
+def submittask(request):
+	transaction_id = request.GET['transid']
+	no_of_hours = request.GET['hourscount']
+	job_number_choice = request.GET['entryname']
+	event = UpdateEvent(transaction_id=transaction_id,no_of_hours=no_of_hours,job_number_choice=job_number_choice);
+	event.save()
+	return HttpResponse("transaction_id is "+transaction_id);
